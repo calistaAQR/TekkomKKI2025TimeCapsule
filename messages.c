b9ff9cd1b2c1359eaa3cceabe4e74e00c9973ec3
calistaAQR <calistaaqr09@gmail.com>
2025-11-28 22:25:32 +0700

    Add calista function to display a message

--- messages.c
+++ messages.c
@@ -6,12 +6,17 @@ void oscar() {
     puts("0006347853 | Oscar Oscar | Just you wait");
 }
 
+// Your message function (Cal's version).
+void calista() {
+    puts("NPMKAMU | Calista Aqila Ramadhani | This is my message");
+    // isi message bebas, asal 1 baris string
+}
+
 int main() {
     oscar();
 
     // Call your function after this.
-
-
+    calista();
 
     return 0;
 }
