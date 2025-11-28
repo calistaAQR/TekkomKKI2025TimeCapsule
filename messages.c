15663945cd6b16e0525512bfc1864e858dcb1a10
calistaAQR <calistaaqr09@gmail.com>
2025-11-28 22:42:39 +0700

    Add my message function

--- messages.c
+++ messages.c
@@ -9,6 +9,10 @@ void oscar() {
 // Your message function (Cal's version).
 void calista() {
     puts("2506557791 | Calista Aqila Ramadhani | PagiTeknik!_MALAM");
+<<<<<<< HEAD
+=======
+    // isi message bebas, asal 1 baris string
+>>>>>>> 3e6e06d4c018cd474387c040f9a20ffe2de8ba7c
 }
 
 int main() {
